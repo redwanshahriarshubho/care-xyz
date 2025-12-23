f018d734306bba2678aa53a7904ebe26ae553767
Redwan Shahriar Shubho <142111295+redwanshahriarshubho@users.noreply.github.com>
2025-12-24 01:31:34 +0600

    updated

--- next.config.js
+++ next.config.js
@@ -1,8 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,        // React strict mode enabled
+  swcMinify: true,              // Use SWC compiler for faster builds and minification
+
   experimental: {
-    serverActions: true
-  }
+    serverActions: {}           // Correct format: empty object instead of boolean
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',          // Allow all remote images (change if you want to restrict)
+      },
+    ],
+  },
+
+  eslint: {
+    ignoreDuringBuilds: true,    // Prevent ESLint warnings from breaking the build
+  },
+
+  compiler: {
+    styledComponents: true,      // If using styled-components
+  },
 };
 
 module.exports = nextConfig;
