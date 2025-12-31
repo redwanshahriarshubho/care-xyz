d84416e543e627d6d573ee6c0f37328f0449facf
Redwan Shahriar Shubho <142111295+redwanshahriarshubho@users.noreply.github.com>
2025-12-31 22:07:50 +0600

    Fix config and package file

--- next.config.js
+++ next.config.js
@@ -1,28 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,        // React strict mode enabled
-  swcMinify: true,              // Use SWC compiler for faster builds and minification
-
-  experimental: {
-    serverActions: {}           // Correct format: empty object instead of boolean
-  },
-
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',          // Allow all remote images (change if you want to restrict)
-      },
-    ],
-  },
-
-  eslint: {
-    ignoreDuringBuilds: true,    // Prevent ESLint warnings from breaking the build
-  },
-
-  compiler: {
-    styledComponents: true,      // If using styled-components
-  },
-};
+  reactStrictMode: true,
+  swcMinify: true
+}
 
 module.exports = nextConfig;
